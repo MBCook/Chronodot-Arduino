b20e78cd108104787cc8de609410fe6cae0309c6
Michael Cook <foobarsoft@foobarsoft.com>
2013-06-10 11:05:04 -0500

    Starting to move strings into flash to save SRAM, need to convert arrays at start of program

--- examples/chronodot_test/chronodot_test.ino
+++ examples/chronodot_test/chronodot_test.ino
@@ -38,20 +38,20 @@ void loop() {
 	
 	while (true) {	
   
-		Serial.println("");
-		Serial.println("Main Menu");
-		Serial.println("---------");
-		Serial.println("1. Show time");
-		Serial.println("2. Show status");	
-		Serial.println("3. Change time");
-		Serial.println("4. Alarm settings");
-		Serial.println("5. General settings");
-		Serial.println("");
+		Serial.println(F(""));
+		Serial.println(F("Main Menu"));
+		Serial.println(F("---------"));
+		Serial.println(F("1. Show time"));
+		Serial.println(F("2. Show status"));	
+		Serial.println(F("3. Change time"));
+		Serial.println(F("4. Alarm settings"));
+		Serial.println(F("5. General settings"));
+		Serial.println(F(""));
 		
 		lastInput = 0;
 		
 		while (lastInput == 0) {
-			Serial.print("? ");
+			Serial.print(F("? "));
 			
 			while (Serial.available() == 0) {
 				// Just wait
@@ -64,7 +64,7 @@ void loop() {
 			while (Serial.available() > 0)	// Eat up any other input
 				Serial.read();
 				
-			Serial.println("");
+			Serial.println(F(""));
 				
 			switch (lastInput) {
 				case '1':
@@ -107,7 +107,7 @@ void showTime() {
 		
 	Serial.print(daysOfWeek[dayOfWeek]);
 	
-	Serial.print(" ");
+	Serial.print(F(" "));
 	
 	uint8_t month = myTime.getMonth();
 	
@@ -116,29 +116,29 @@ void showTime() {
 		
 	Serial.print(monthNames[month]);
 	
-	Serial.print(" ");
+	Serial.print(F(" "));
 	
 	Serial.print(myTime.getDayOfMonth());
 	
-	Serial.print(", ");
+	Serial.print(F(", "));
 	
 	Serial.print(2000 + myTime.getYear());
 	
-	Serial.print(" - ");
+	Serial.print(F(" - "));
 	
 	Serial.print(myTime.getHours());
 	
-	Serial.print(":");
+	Serial.print(F(":"));
 	
 	if (myTime.getMinutes() < 10)
-		Serial.print("0");
+		Serial.print(F("0"));
 	
 	Serial.print(myTime.getMinutes());
 	
-	Serial.print(":");
+	Serial.print(F(":"));
 	
 	if (myTime.getSeconds() < 10)
-		Serial.print("0");
+		Serial.print(F("0"));
 		
 	Serial.println(myTime.getSeconds());
 }
@@ -148,82 +148,82 @@ void showTime() {
 /////////////////////////////////////////////
 
 void showStatus() {
-	Serial.println("");
+	Serial.println(F(""));
 
 	bool batteryOscillator = Chronodot::getOscillatorEnabled();
 	
-	Serial.print("Oscillator enabled on battery is ");
+	Serial.print(F("Oscillator enabled on battery is "));
 	Serial.println(batteryOscillator);
 
 	bool batterySquarewave = Chronodot::getBatterySquareWaveEnabled();
 	
-	Serial.print("Battery backed square wave enabled is ");
+	Serial.print(F("Battery backed square wave enabled is "));
 	Serial.println(batterySquarewave);
 	
 	uint8_t speed = Chronodot::getOutputSquarewaveSpeed();
 	
 	switch (speed) {
 		case SPEED_1_HZ:
-			Serial.println("Square wave output is 1 Hz");
+			Serial.println(F("Square wave output is 1 Hz"));
 			break;
 		case SPEED_1_024_KHZ:
-			Serial.println("Square wave output is 1.024 kHz");
+			Serial.println(F("Square wave output is 1.024 kHz"));
 			break;
 		case SPEED_4_096_KHZ:
-			Serial.println("Square wave output is 4.096 kHz");
+			Serial.println(F("Square wave output is 4.096 kHz"));
 			break;
 		case SPEED_8_192_KHZ:
-			Serial.println("Square wave output is 8.192 kHz");
+			Serial.println(F("Square wave output is 8.192 kHz"));
 			break;
 		default:
-			Serial.println("Square wave off, interupt on alarm");
+			Serial.println(F("Square wave off, interupt on alarm"));
 	}
 	
 	bool thirtyTwo = Chronodot::get32kHzOutputEnabled();
 	
-	Serial.print("32kHz otuput enabled is ");
+	Serial.print(F("32kHz otuput enabled is "));
 	Serial.println(thirtyTwo);
 	
 	bool oscillatorStop = Chronodot::getOscillatorStop();
 	
-	Serial.print("Oscillator was stopped is ");
+	Serial.print(F("Oscillator was stopped is "));
 	Serial.println(oscillatorStop);
 	
 	bool busy = Chronodot::isBusy();
 	
-	Serial.print("Busy flag is ");
+	Serial.print(F("Busy flag is "));
 	Serial.println(busy);
 	
 	bool alarm = Chronodot::getAlarmOneEnabled();
 	
-	Serial.print("Alarm one enabled is ");
+	Serial.print(F("Alarm one enabled is "));
 	Serial.println(alarm);
 	
 	alarm = Chronodot::getAlarmOneFired();
 	
-	Serial.print("Alarm one fired is ");
+	Serial.print(F("Alarm one fired is "));
 	Serial.println(alarm);
 	
 	alarm = Chronodot::getAlarmTwoEnabled();
 	
-	Serial.print("Alarm two enabled is ");
+	Serial.print(F("Alarm two enabled is "));
 	Serial.println(alarm);
 	
 	alarm = Chronodot::getAlarmTwoFired();
 	
-	Serial.print("Alarm two fired is ");
+	Serial.print(F("Alarm two fired is "));
 	Serial.println(alarm);
 	
 	int8_t agingOffset = Chronodot::getAgingOffset();
 	
-	Serial.print("Aging offset is ");
+	Serial.print(F("Aging offset is "));
 	Serial.println(agingOffset);
 	
 	float temp = Chronodot::getTemperature();
 	
-	Serial.print("Temperature is ");
+	Serial.print(F("Temperature is "));
 	Serial.print(temp);
-	Serial.println(" C");
+	Serial.println(F(" C"));
 }
 
 ///////////////////////////////
@@ -233,20 +233,20 @@ void showStatus() {
 void changeTime() {
 	char buffer[100];
 
-	Serial.println("");
-	Serial.println("Please enter the time in the form:");
-	Serial.println("");
-	Serial.println("7, 1/24/05 23:18:12 d");
-	Serial.println("");
+	Serial.println(F(""));
+	Serial.println(F("Please enter the time in the form:"));
+	Serial.println(F(""));
+	Serial.println(F("7, 1/24/05 23:18:12 d"));
+	Serial.println(F(""));
 	Serial.println("The first digit is the day (1 = Sunday, 7 = Saturday),");
-	Serial.println("use two digits for the year, and use 24h time.");
-	Serial.println("The 'd' is how we know you're done typing.");
-	Serial.println("");
-	Serial.println("Enter 'x' to return to the main menu.");
+	Serial.println(F("use two digits for the year, and use 24h time."));
+	Serial.println(F("The 'd' is how we know you're done typing."));
+	Serial.println(F(""));
+	Serial.println(F("Enter 'x' to return to the main menu."));
 
 	while (true) {
-		Serial.println("");
-		Serial.print("? ");
+		Serial.println(F(""));
+		Serial.print(F("? "));
 	
 		// We're going to gather up characters until we get an 'x', 'd', or buffer is full
 		
@@ -272,8 +272,8 @@ void changeTime() {
 			} else if (buffer[i] == 'x' || buffer[i] == 'X') {
 				return;			// Back to the main menu
 			} else if (i >= 99) {
-				Serial.println("");
-				Serial.println("Input too long.");
+				Serial.println(F(""));
+				Serial.println(F("Input too long."));
 				
 				i = 255;		// Signal that there was a problem
 				
@@ -294,14 +294,12 @@ void changeTime() {
 		uint8_t start = 0;
 		uint8_t nextStart = 0;
 		
-		Serial.println("Input done");
-		
 		for (uint8_t i = 0; i < 7; i++) {
 			numbers[i] = getNextNumber(buffer, start, &nextStart);
 			
 			if (nextStart == 255 && i != 6) {
-				Serial.println("");
-				Serial.println("Didn't get enough numbers");
+				Serial.println(F(""));
+				Serial.println(F("Didn't get enough numbers"));
 				
 				i = 255;
 				
@@ -316,44 +314,40 @@ void changeTime() {
 		
 		// OK, numbers contains all the fields. Do some quick validation
 		
-		Serial.println("Verifying...");
-		
 		if (numbers[0] == 0 || numbers[0] > 7) {
-			Serial.println("");
-			Serial.println("Day of week out of range");
+			Serial.println(F(""));
+			Serial.println(F("Day of week out of range"));
 			
 			continue;
 		} else if (numbers[1] == 0 || numbers[1] > 12) {
-			Serial.println("");
-			Serial.println("Month out of range");
+			Serial.println(F(""));
+			Serial.println(F("Month out of range"));
 			
 			continue;
 		} else if (numbers[2] == 0 || numbers[2] > 31) {
-			Serial.println("");
-			Serial.println("Day of month out of range");
+			Serial.println(F(""));
+			Serial.println(F("Day of month out of range"));
 			
 			continue;
 		} else if (numbers[4] >= 24) {
-			Serial.println("");
-			Serial.println("Hours out of range");
+			Serial.println(F(""));
+			Serial.println(F("Hours out of range"));
 			
 			continue;
 		} else if (numbers[5] >= 59) {
-			Serial.println("");
-			Serial.println("Minutes out of range");
+			Serial.println(F(""));
+			Serial.println(F("Minutes out of range"));
 			
 			continue;
 		} else if (numbers[6] >= 59) {
 
-			Serial.println("");
-			Serial.println("Seconds out of range");
+			Serial.println(F(""));
+			Serial.println(F("Seconds out of range"));
 			
 			continue;
 		}
 		
 		// OK, that's it. Update our time.
-		
-		Serial.println("Preparing...");
 		
 		myTime.setDayOfWeek(numbers[0]);
 		myTime.setMonth(numbers[1]);
@@ -363,14 +357,12 @@ void changeTime() {
 		myTime.setMinutes(numbers[5]);
 		myTime.setSeconds(numbers[6]);
 		
-		Serial.println("Updating...");
-		
 		Chronodot::setTime(&myTime);
 	
 		// Show them the result, go back to main menu
 	
-		Serial.println("");
-		Serial.print("Time is now: ");
+		Serial.println(F(""));
+		Serial.print(F("Time is now: "));
 		
 		showTime();
 		
@@ -389,7 +381,6 @@ uint8_t getNextNumber(char *data, uint8_t start, uint8_t *nextStart) {
 			foundNumbers = true;
 			working = working * 10 + (data[i] - '0');
 		} else if (data[i] == 0 || data[i] == 'd' || data[i] == 'D') {
-			Serial.println("\tFound end of string terminator");
 			// End of the string
 			
 			*nextStart = 255;
@@ -425,21 +416,21 @@ void alarmMenu() {
 
 void alarmSettings() {
 	while (true) {
-		Serial.println("");
-		Serial.println("Alarm Settings");
-		Serial.println("---------");
-		Serial.println("1. Toggle alarm one enabled");
-		Serial.println("2. Toggle alarm two enabled");	
-		Serial.println("3. Reset alarm one flag");
-		Serial.println("4. Reset alarm two flag");
-		Serial.println("");
-		Serial.println("x. Back to main menu");
-		Serial.println("");
+		Serial.println(F(""));
+		Serial.println(F("Alarm Settings"));
+		Serial.println(F("---------"));
+		Serial.println(F("1. Toggle alarm one enabled"));
+		Serial.println(F("2. Toggle alarm two enabled"));	
+		Serial.println(F("3. Reset alarm one flag"));
+		Serial.println(F("4. Reset alarm two flag"));
+		Serial.println(F(""));
+		Serial.println(F("x. Back to main menu"));
+		Serial.println(F(""));
 	
 		lastInput = 0;
 	
 		while (lastInput == 0) {
-			Serial.print("? ");
+			Serial.print(F("? "));
 		
 			while (Serial.available() == 0) {
 				// Just wait
@@ -452,7 +443,7 @@ void alarmSettings() {
 			while (Serial.available() > 0)	// Eat up any other input
 				Serial.read();
 			
-			Serial.println("");
+			Serial.println(F(""));
 			
 			bool old;		// Not allowed in the switch with this compiler
 			
@@ -462,7 +453,7 @@ void alarmSettings() {
 					
 					Chronodot::enableAlarmOne(!old);
 					
-					Serial.print("Alarm one enabled is now ");
+					Serial.print(F("Alarm one enabled is now "));
 					Serial.println(!old);
 					
 					break;
@@ -471,20 +462,20 @@ void alarmSettings() {
 					
 					Chronodot::enableAlarmTwo(!old);
 					
-					Serial.print("Alarm two enabled is now ");
+					Serial.print(F("Alarm two enabled is now "));
 					Serial.println(!old);
 					
 					break;
 				case '3':
 					Chronodot::resetAlarmOneFired();
 					
-					Serial.println("Alarm one flag reset");
+					Serial.println(F("Alarm one flag reset"));
 					
 					break;
 				case '4':
 					Chronodot::resetAlarmTwoFired();
 					
-					Serial.println("Alarm two flag reset");
+					Serial.println(F("Alarm two flag reset"));
 
 					break;
 				case 'x':
@@ -503,27 +494,27 @@ void alarmSettings() {
 
 void generalSettings() {
 	while (true) {
-		Serial.println("");
-		Serial.println("General Settings");
-		Serial.println("---------");
-		Serial.println("1. Toggle battery oscillator");
-		Serial.println("2. Toggle battery square wave");	
-		Serial.println("3. Trigger temperature reading");
-		Serial.println("4. Set interrupt on alarm");
-		Serial.println("5. Output 1 Hz square wave");
-		Serial.println("6. Output 1.024 kHz square wave");
-		Serial.println("7. Output 4.096 kHz square wave");
-		Serial.println("8. Output 8.196 kHz square wave");
-		Serial.println("9. Reset oscillator stop");
-		Serial.println("0. Toggle 32 kHz out");
-		Serial.println("");
-		Serial.println("x. Back to main menu");
-		Serial.println("");
+		Serial.println(F(""));
+		Serial.println(F("General Settings"));
+		Serial.println(F("---------"));
+		Serial.println(F("1. Toggle battery oscillator"));
+		Serial.println(F("2. Toggle battery square wave"));	
+		Serial.println(F("3. Trigger temperature reading"));
+		Serial.println(F("4. Set interrupt on alarm"));
+		Serial.println(F("5. Output 1 Hz square wave"));
+		Serial.println(F("6. Output 1.024 kHz square wave"));
+		Serial.println(F("7. Output 4.096 kHz square wave"));
+		Serial.println(F("8. Output 8.196 kHz square wave"));
+		Serial.println(F("9. Reset oscillator stop"));
+		Serial.println(F("0. Toggle 32 kHz out"));
+		Serial.println(F(""));
+		Serial.println(F("x. Back to main menu"));
+		Serial.println(F(""));
 	
 		lastInput = 0;
 	
 		while (lastInput == 0) {
-			Serial.print("? ");
+			Serial.print(F("? "));
 		
 			while (Serial.available() == 0) {
 				// Just wait
@@ -536,7 +527,7 @@ void generalSettings() {
 			while (Serial.available() > 0)	// Eat up any other input
 				Serial.read();
 			
-			Serial.println("");
+			Serial.println(F(""));
 			
 			bool old;		// Not allowed in the switch with this compiler
 			
@@ -546,7 +537,7 @@ void generalSettings() {
 					
 					Chronodot::enableOscillator(!old);
 					
-					Serial.print("Battery oscillator is now ");
+					Serial.print(F("Battery oscillator is now "));
 					Serial.println(!old);
 					
 					break;
@@ -555,54 +546,54 @@ void generalSettings() {
 					
 					Chronodot::enableBatterySquareWave(!old);
 					
-					Serial.print("Battery square wave is now ");
+					Serial.print(F("Battery square wave is now "));
 					Serial.println(!old);
 					
 					break;
 				case '3':
 					Chronodot::senseTemperature(true);
 					
-					Serial.println("Temperature reading started");
+					Serial.println(F("Temperature reading started"));
 
-					Serial.print("Temperature is ");
+					Serial.print(F("Temperature is "));
 					Serial.print(Chronodot::getTemperature());
-					Serial.println(" C");
+					Serial.println(F(" C"));
 					
 					break;
 				case '4':
 					Chronodot::outputInterruptOnAlarm();
 					
-					Serial.println("Interrupt on alarm set");
+					Serial.println(F("Interrupt on alarm set"));
 
 					break;
 				case '5':
 					Chronodot::outputSquarewaveSpeed(SPEED_1_HZ);
 					
-					Serial.println("Squarewave output is 1 Hz");
+					Serial.println(F("Squarewave output is 1 Hz"));
 
 					break;
 				case '6':
 					Chronodot::outputSquarewaveSpeed(SPEED_1_024_KHZ);
 					
-					Serial.println("Squarewave output is 1.024 kHz");
+					Serial.println(F("Squarewave output is 1.024 kHz"));
 
 					break;				
 				case '7':
 					Chronodot::outputSquarewaveSpeed(SPEED_4_096_KHZ);
 					
-					Serial.println("Squarewave output is 4.096 kHz");
+					Serial.println(F("Squarewave output is 4.096 kHz"));
 
 					break;				
 				case '8':
 					Chronodot::outputSquarewaveSpeed(SPEED_8_192_KHZ);
 					
-					Serial.println("Squarewave output is 8.192 kHz");
+					Serial.println(F("Squarewave output is 8.192 kHz"));
 
 					break;				
 				case '9':
 					Chronodot::resetOscillatorStop();
 					
-					Serial.println("Oscillator stop flag reset");
+					Serial.println(F("Oscillator stop flag reset"));
 
 					break;
 				case '0':
@@ -610,7 +601,7 @@ void generalSettings() {
 					
 					Chronodot::enable32kHzOutput(!old);
 					
-					Serial.print("32kHz output enabled is ");
+					Serial.print(F("32kHz output enabled is "));
 					Serial.println(!old);
 					
 					break;
